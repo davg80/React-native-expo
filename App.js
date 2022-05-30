4966cffbffdb36592a25c5b226682f4ffa316e0c
dg80 <dguillin80@gmail.com>
2022-05-30 09:20:21 +0200

    show planets list

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
+import Planets from './components/Planets';
 
 export default function App() {
+  const [planets, setPlanets] = useState([]);
+
+  useEffect(() => {
+    fetch("https://swapi.dev/api/planets/")
+      .then((response) => response.json())
+      .then((data) => {
+        setPlanets(data.results);
+      })
+      .catch(error => console.log(error))
+  }, [])
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title} >Les planètes</Text>
+      { planets.length > 0 ?
+        <Planets planets={planets} /> : 
+        <ActivityIndicator size="small" color="#0000ff" />
+      }
     </View>
   );
 }
@@ -17,4 +32,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    fontSize: 20,
+    marginBottom: 10
+  },
 });
+
+
+
+
+
+
