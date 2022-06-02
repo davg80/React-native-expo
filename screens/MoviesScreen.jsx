80074f798c7976b8dd2a6f82c2bec9cc2d211065
dg80 <dguillin80@gmail.com>
2022-06-02 21:18:57 +0200

    Add Image post movies

--- screens/MoviesScreen.jsx
+++ screens/MoviesScreen.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, ScrollView, Image, Dimensions } from 'react-native';
 import HeaderComponent from '../components/HeaderComponent';
 import { useFirebase } from '../Hooks/useFirebase';
 import { useMovies } from '../Hooks/useMovies';
@@ -8,27 +8,31 @@ const RED = "#f92045";
 const WHITE = "#F1F1F1";
 const LIGHT_GRAY = "#D3D3D3";
 const HomeScreen = () => {
+    const BASE_URL_IMAGE = "https://image.tmdb.org/t/p/w200";
     const { user } = useFirebase();
     const { movies } = useMovies();
     console.log(movies);
 
-    const listMovies = () => {
-
-    };
     return (
         <View style={styles.containerMovies}>
             <HeaderComponent user={user}></HeaderComponent>
-            {
-                movies.map((movie) => {
-                    return (
-                        <View key={movie.id} style={{ margin: 10 }}>
-                            <Text>{movie.title}</Text>
-                            <Text>{movie.overview}</Text>
-                        </View>
-                    );
-                })
-            }
-        </View>
+            <ScrollView showsVerticalScrollIndicator={false}>
+                {
+                    movies.map((movie) => {
+                        return (
+                            <View key={movie.id} style={styles.cardMovie}>
+                                <Image
+                                    source={{ uri: `${BASE_URL_IMAGE}${movie.poster_path}` }}
+                                    style={styles.poster}
+                                />
+                                <Text style={styles.cardTitle}>{movie.title}</Text>
+                                <Text style={styles.cardOverview}>{movie.overview}</Text>
+                            </View>
+                        );
+                    })
+                }
+            </ScrollView >
+        </View >
     );
 }
 
@@ -51,6 +55,28 @@ const styles = StyleSheet.create({
     avatar: {
         width: 40,
         height: 40,
+    },
+    cardMovie: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: RED,
+        marginBottom: 25,
+        padding: 30,
+    },
+    poster: {
+        width: 350,
+        height: 500,
+        borderRadius: 15,
+    },
+    cardTitle: {
+        fontSize: 22,
+        fontWeight: 'bold',
+        color: WHITE
+    },
+    cardOverview: {
+        color: WHITE,
+        textAlign: 'justify'
     }
 });
 
